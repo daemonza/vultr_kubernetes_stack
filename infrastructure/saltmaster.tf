5cae847188c157133cb0d5ff37d9c1f64da37174
Werner Gillmer <werner.gillmer@bloomreach.com>
2017-03-14 14:05:57 +0100

    bootstrap of salt master and minion communicating

--- infrastructure/saltmaster.tf
+++ infrastructure/saltmaster.tf
@@ -12,11 +12,11 @@ resource "vultr_server" "k8s" {
 	name = "master"
 
 	# amsterdam. Find regions with `vultr regions`
-	region_id = 7
+	region_id = 7 
 
-	# smallest vultr server. Get plans with `vultr plans`
+	# Get plans with `vultr plans`
 	# For a production cluster, a bigger plan needs to be used.
-	plan_id = 87 
+	plan_id = 201 
 
 	# The operating system you want to use. 167 is CentOS 7.
 	# Get a list of OS's with `vultr os`
@@ -31,16 +31,45 @@ resource "vultr_server" "k8s" {
 	# enable one or more ssh keys on the root account.
 	ssh_key_ids = ["${vultr_ssh_key.k8s.id}"]
 
-	# get the IP address of the server that got created 
-	provisioner "local-exec" {
-        command = "echo local-exec ${vultr_server.k8s.ipv4_address}"
-    }
+	# map the IP to "salt" so that the minion can find the salt master 
+	provisioner "remote-exec" {
+        inline = [ 
+		   "echo '${vultr_server.k8s.ipv4_address} salt' >> /etc/hosts"
+		 ]
+         }
 
-	# bootstrap salt master 
+	# change hostname to master. This will set the minion id as master.
+	# Seeing as this minion controls the master, I like to call it master.
+	provisioner "remote-exec" {
+        inline = [ 
+		   "hostname master"
+		 ]
+         }
+
+	# bootstrap salt 
 	provisioner "remote-exec" {
         inline = [
                   "curl -o bootstrap-salt.sh -L https://bootstrap.saltstack.com",
-                  "sh bootstrap-salt.sh -M -N git develop",
+                  "sh bootstrap-salt.sh -M -P git develop",
 		]
-    }
+         }
+
+	# setup salt keys
+        provisioner "remote-exec" {
+        inline = [ 
+		   "mkdir -p /etc/salt/pki/master/minions",
+                   "salt-key --gen-keys=minion --gen-keys-dir=/etc/salt/pki/minion",
+                   "mkdir -p /etc/salt/pki/minion",
+                   "cp /etc/salt/pki/minion/minion.pub /etc/salt/pki/master/minions/master",
+		 ]
+         }
+
+	# restart the salt 
+	provisioner "remote-exec" {
+        inline = [
+                  "systemctl restart salt-minion.service",
+                  "systemctl restart salt-master.service",
+		]
+         }
+
 }
