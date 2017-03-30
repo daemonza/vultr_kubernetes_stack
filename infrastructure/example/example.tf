78d8c3172f35f76e27b85cbd935c7dc9b1b6a08c
Werner Gillmer <werner.gillmer@bloomreach.com>
2017-03-30 19:49:47 +0200

    stash before new macbook

---
+++ infrastructure/example/example.tf
@@ -0,0 +1,52 @@
+#provider "vultr" {
+#	api_key = "TODO_SET_TO_YOUR_API_KEY__OR__THE_VULTR_API_KEY_ENV_VARIABLE"
+#}
+
+resource "vultr_ssh_key" "example" {
+	name = "example created from terraform"
+
+	# get the public key from a local file.
+	#
+	# create the example_rsa.pub file with:
+	#
+	#	ssh-keygen -t rsa -b 4096 -C 'terraform example' -f example_rsa -N ''
+	public_key = "${file("example_rsa.pub")}"
+}
+
+resource "vultr_server" "example" {
+	name = "example created from terraform"
+
+	# set the region. 7 is Amsterdam.
+	# get the list of regions with the command: vultr regions
+	region_id = 7
+
+	# set the plain. 29 is 768 MB RAM,15 GB SSD,1.00 TB BW.
+	# get the list of plans with the command: vultr plans --region 7
+	plan_id = 87 
+
+	# set the OS image. 179 is CoreOS Stable.
+	# get the list of OSs with the command: vultr os
+	os_id = 179
+
+	# enable IPv6.
+	ipv6 = true
+
+	# enable private networking.
+	private_networking = true
+
+	# enable one or more ssh keys on the root account.
+	ssh_key_ids = ["${vultr_ssh_key.example.id}"]
+
+	# execute a command on the local machine.
+	provisioner "local-exec" {
+        command = "echo local-exec ${vultr_server.example.ipv4_address}"
+    }
+
+	# execute commands on the remote machine.
+	provisioner "remote-exec" {
+        inline = [
+			"env",
+			"ip addr",
+		]
+    }
+}
--- infrastructure/saltmaster.tf
+++ infrastructure/saltmaster.tf
@@ -2,13 +2,13 @@ provider "vultr" {
 	api_key = "${var.vultr_api_key}"
 }
 
-resource "vultr_ssh_key" "k8s" {
+resource "vultr_ssh_key" "cluster" {
 	name = "master node ssh key"
 	public_key = "${file("./keys/id_rsa.pub")}"
 }
 
 # This is the bootstrapped Salt master.
-resource "vultr_server" "k8s" {
+resource "vultr_server" "master" {
 	name = "master"
 
 	# amsterdam. Find regions with `vultr regions`
@@ -26,15 +26,15 @@ resource "vultr_server" "k8s" {
 	ipv6 = true
 
 	# enable private networking.
-	private_networking = true
+	private_networking = false 
 
 	# enable one or more ssh keys on the root account.
-	ssh_key_ids = ["${vultr_ssh_key.k8s.id}"]
+	ssh_key_ids = ["${vultr_ssh_key.cluster.id}"]
 
 	# map the IP to "salt" so that the minion can find the salt master 
 	provisioner "remote-exec" {
         inline = [ 
-		   "echo '${vultr_server.k8s.ipv4_address} salt' >> /etc/hosts"
+		   "echo '${vultr_server.master.ipv4_address} salt' >> /etc/hosts"
 		 ]
          }
 
@@ -69,6 +69,71 @@ resource "vultr_server" "k8s" {
         inline = [
                   "systemctl restart salt-minion.service",
                   "systemctl restart salt-master.service",
+		]
+         }
+
+}
+
+# Minion.
+resource "vultr_server" "minion1" {
+	name = "minion1"
+
+	# amsterdam. Find regions with `vultr regions`
+	region_id = 7 
+
+	# Get plans with `vultr plans`
+	# For a production cluster, a bigger plan needs to be used.
+	plan_id = 201 
+
+	# The operating system you want to use. 167 is CentOS 7.
+	# Get a list of OS's with `vultr os`
+	os_id = 167
+
+	# enable IPv6.
+	ipv6 = true
+
+	# enable private networking.
+	private_networking = false 
+
+	# enable one or more ssh keys on the root account.
+	ssh_key_ids = ["${vultr_ssh_key.cluster.id}"]
+
+	# map the master IP to "salt" so that the minion can find the salt master 
+	provisioner "remote-exec" {
+        inline = [ 
+		   "echo '${vultr_server.master.ipv4_address} salt' >> /etc/hosts"
+		 ]
+         }
+
+	# set the hostname. This get's used by minion id 
+	provisioner "remote-exec" {
+        inline = [ 
+		   "hostname minion1"
+		 ]
+         }
+
+	# bootstrap salt minion only 
+	provisioner "remote-exec" {
+        inline = [
+                  "curl -o bootstrap-salt.sh -L https://bootstrap.saltstack.com",
+                  "sh bootstrap-salt.sh -P git develop",
+		]
+         }
+
+	# setup salt keys
+  #  provisioner "remote-exec" {
+  #      inline = [ 
+  #	              "mkdir -p /etc/salt/pki/master/minions",
+  #                "salt-key --gen-keys=minion --gen-keys-dir=/etc/salt/pki/minion",
+  #                 "mkdir -p /etc/salt/pki/minion",
+  #                 "cp /etc/salt/pki/minion/minion.pub /etc/salt/pki/master/minions/master",
+  #		      ]
+  #        }
+
+	# restart the salt 
+	provisioner "remote-exec" {
+        inline = [
+                  "systemctl restart salt-minion.service",
 		]
          }
 
